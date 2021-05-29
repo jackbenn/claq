cd199f3841235a35ba9ab3ba74c2460d99351943
Jack Bennetto <jack@bennetto.com>
2021-05-29 11:37:55 -0700

    start of things ort of working

--- src/claq.py
+++ src/claq.py
@@ -1,25 +1,35 @@
 import pandas as pd
 import sys
 import argparse
+import matplotlib.pyplot as plt
 
 
 class Data:
     def __init__(self, df=None):
         self.df = df
 
-    def load(self, stream, sep=','):
+    def load(self, stream, sep=',', header='infer'):
         self.df = pd.read_csv(stream,
                               sep=sep,
-                              header=None)
+                              header=header)
     
     def print(self):
-        self.df.to_csv(sys.stdout)
+        self.df.to_csv(sys.stdout, index=False)
 
     def cut(self, cols):
-        return Data(self.df.iloc[:cols])
+        cols = [int(s) for s in cols.split(',')]
+        return Data(self.df.iloc[:, cols])
     
     def where(self, exp):
-        return Data(self.df[self.df[exp]])
+        return Data(self.df[self.df.apply(exp, axis=1)])
+    
+    def plot(self):
+        self.df.plot(0, 1)
+        plt.show()
+
+    def hist(self):
+        self.df.hist()
+        plt.show()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -28,8 +38,13 @@ if __name__ == '__main__':
     #parser.add_argument('hist', help="Show a histogram")
     #parser.add_argument('plot', help="Show a plot")
     args = parser.parse_args(sys.argv[1:2])
-    print(args)
     data = Data()
     data.load(sys.stdin)
-    data = data.where(lambda x: x[0] > 2)
+    if args.command == 'plot':
+        data.plot()
+    elif args.command == 'hist':
+        data.hist()
+    elif args.command == 'cut':
+        cols = sys.argv[2]
+        data = data.cut(cols)
     data.print()
