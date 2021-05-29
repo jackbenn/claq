7e455ca1a3f9c051bd0015fd21dca858b4ed196c
Jack Bennetto <jack@bennetto.com>
2021-05-28 21:18:13 -0700

    start doing arg parsing (unfinished)

--- src/claq.py
+++ src/claq.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import sys
+import argparse
+
 
 class Data:
     def __init__(self, df=None):
@@ -17,9 +19,16 @@ class Data:
         return Data(self.df.iloc[:cols])
     
     def where(self, exp):
-        return Data(self.df[exp, axis=1))
+        return Data(self.df[self.df[exp]])
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', help='Subcommand to run')
+
+    #parser.add_argument('hist', help="Show a histogram")
+    #parser.add_argument('plot', help="Show a plot")
+    args = parser.parse_args(sys.argv[1:2])
+    print(args)
     data = Data()
     data.load(sys.stdin)
     data = data.where(lambda x: x[0] > 2)
